9ed00bbb4d5194b18742eb704a413a5cedadafc3
sugarvoid <gamedev@kitargo.com>
2024-12-01 09:03:06 -0600

    Remove light sprite

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ rm bin/res/*
 cp -a res/* bin/res/
 
 gcc ./src/main.c -o ./bin/redgreen -O2 -Wall -Wno-unused-result -Wno-missing-braces -I ./include/ -L ./lib/ -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 
-./bin/redgreen
+strip ./bin/redgreen
